f2e4c609a2b248fbd036d11d93d3923fc0326e52
sander1 <sanderspies+plexapp@gmail.com>
2011-03-25 04:00:31 +0100

    Leave out items that have no video

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,4 @@
-import datetime, re, pickle
+import datetime, re
 
 PLUGIN_PREFIX   = "/video/cnettv"
 ROOT_URL        = "http://cnettv.cnet.com/"
@@ -6,84 +6,92 @@ SEARCH_API_URL  = "http://api.cnet.com/restApi/v1.0/videoSearch?%s=%s"
 CNET_PARMS      = "&orderBy=productionDate~desc,createDate~desc&limit=20&iod=images,videoMedia,relatedLink,breadcrumb,relatedAssets,broadcast%2Clowcache&partTag=cntv&showBroadcast=true"
 CNET_NAMESPACE  = {'l':'http://api.cnet.com/rest/v1.0/ns'}
 PARAM_NAME_MAP  = {'videoId':'videoIds', 'node':'categoryIds', 'videoProfileIds':'franchiseIds', 'videoProfileId':'franchiseIds'}
+
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "CNET TV", "icon-default.png", "art-default.png")
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "CNET TV", "icon-default.png", "art-default.jpg")
   Plugin.AddViewGroup("Details", viewMode="InfoList", mediaType="items")
-  MediaContainer.art = R('art-default.png')
+  MediaContainer.art = R('art-default.jpg')
   MediaContainer.title1 ="CNET TV"
   DirectoryItem.thumb=R("icon-default.png")
-  
 
 #####################################  
 def MainMenu():
-    dir = MediaContainer() 
-    videoId = TodaysVideoId()
-    dir.Append(Function(DirectoryItem(Videos, title='Today on CNET'), key='videoIds', params=videoId))
-    for item in HTML.ElementFromURL(ROOT_URL).xpath('//ul[@id="mainMenu"]/li'):
-      title = item.find('a').text_content()
-      subMenus = []
-      for subItem in item.xpath('ul/li/a'):
-        try:
-          onClickItems = [p.strip("'") for p in re.findall("'[^']+'", subItem.get('onclick'))]
-          onClickItems[0] = onClickItems[0].replace(u'\x92',"'")
-          subMenus += [onClickItems]
-        except:
-          pass
-      
-      if len(subMenus) > 0:
-      	dir.Append(Function(DirectoryItem(Menu, title), subMenus=subMenus))
-    return dir
+  dir = MediaContainer() 
+  videoId = TodaysVideoId()
+  dir.Append(Function(DirectoryItem(Videos, title='Today on CNET'), key='videoIds', params=videoId))
+
+  for item in HTML.ElementFromURL(ROOT_URL).xpath('//ul[@id="mainMenu"]/li'):
+    title = item.find('a').text_content()
+    subMenus = []
+    for subItem in item.xpath('./ul/li/a'):
+      try:
+        onClickItems = [p.strip("'") for p in re.findall("'[^']+'", subItem.get('onclick'))]
+        onClickItems[0] = onClickItems[0].replace(u'\x92',"'")
+        subMenus += [onClickItems]
+      except:
+        pass
+
+    if len(subMenus) > 0:
+      dir.Append(Function(DirectoryItem(Menu, title), subMenus=subMenus))
+
+  return dir
 
 #####################################
 def Menu(sender, subMenus):
-	dir = MediaContainer()
-	for subMenu in subMenus:
-		title = unicode(subMenu[0])
-		key = PARAM_NAME_MAP[subMenu[1]]
-		params = subMenu[2]
-		dir.Append(Function(DirectoryItem(Videos, title), key=key, params=params))
-	return dir
+  dir = MediaContainer()
 
+  for subMenu in subMenus:
+    title = unicode(subMenu[0])
+    key = PARAM_NAME_MAP[subMenu[1]]
+    params = subMenu[2]
+    dir.Append(Function(DirectoryItem(Videos, title), key=key, params=params))
+
+  return dir
 
 #####################################
 def Videos(sender, key, params):
-   dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle)
-   searchUrl = SEARCH_API_URL % (key, params) + CNET_PARMS
-   for video in XML.ElementFromURL(searchUrl).xpath('/l:CNETResponse/l:Videos/l:Video', namespaces=CNET_NAMESPACE):
-			title = video.xpath('l:Title', namespaces=CNET_NAMESPACE)[0].text
-			summary = video.xpath('l:Description', namespaces=CNET_NAMESPACE)[0].text
-			thumb = pickThumb(video.xpath('l:Images/l:Image', namespaces=CNET_NAMESPACE))
-			duration = int(video.xpath('l:LengthSecs', namespaces=CNET_NAMESPACE)[0].text)*1000
-			media = pickVideo(video.xpath('l:VideoMedias/l:VideoMedia', namespaces=CNET_NAMESPACE))
-			subtitle = Datetime.ParseDate(video.xpath('l:CreateDate', namespaces=CNET_NAMESPACE)[0].text).strftime('%a %b %d, %Y')
-			dir.Append(VideoItem(media, title, subtitle=subtitle, summary=summary, duration=duration, thumb=thumb))
-   return dir
+  dir = MediaContainer(viewGroup='Details', title2=sender.itemTitle)
+  searchUrl = SEARCH_API_URL % (key, params) + CNET_PARMS
+
+  for video in XML.ElementFromURL(searchUrl).xpath('//l:Videos/l:Video', namespaces=CNET_NAMESPACE):
+    # Only process media items that have video
+    if len(video.xpath('./l:VideoMedias', namespaces=CNET_NAMESPACE)) > 0:
+      media = pickVideo(video.xpath('./l:VideoMedias/l:VideoMedia', namespaces=CNET_NAMESPACE))
+      title = video.xpath('./l:Title', namespaces=CNET_NAMESPACE)[0].text
+      summary = video.xpath('./l:Description', namespaces=CNET_NAMESPACE)[0].text
+      thumb = pickThumb(video.xpath('./l:Images/l:Image', namespaces=CNET_NAMESPACE))
+      duration = int(video.xpath('./l:LengthSecs', namespaces=CNET_NAMESPACE)[0].text)*1000
+      subtitle = Datetime.ParseDate(video.xpath('./l:CreateDate', namespaces=CNET_NAMESPACE)[0].text).strftime('%a %b %d, %Y')
+      dir.Append(VideoItem(media, title, subtitle=subtitle, summary=summary, duration=duration, thumb=thumb))
+
+  return dir
 
 #####################################
 def TodaysVideoId():
-   for script in HTML.ElementFromURL(ROOT_URL).xpath('//script'):
-      if script.text != None:
-         start = script.text.find('todaysPlaylist')
-         if start != -1:
-            matches = re.findall(r'[0-9,]+[0-9]+', script.text)
-            if len(matches) > 0:
-               videoId = matches[0]
-               return videoId
-   return None
-   
+  for script in HTML.ElementFromURL(ROOT_URL).xpath('//script'):
+    if script.text != None:
+      start = script.text.find('todaysPlaylist')
+      if start != -1:
+        matches = re.findall(r'[0-9,]+[0-9]+', script.text)
+        if len(matches) > 0:
+          videoId = matches[0]
+          return videoId
+
+  return None
+
 ###################################   
 def pickVideo(videos):
   pickedBitrate = 0
   pickedURL = None
   for video in videos:
-    bitrate = int(video.xpath('l:BitRate', namespaces=CNET_NAMESPACE)[0].text)
+    bitrate = int(video.xpath('./l:BitRate', namespaces=CNET_NAMESPACE)[0].text)
     if bitrate > pickedBitrate:
-      pickedURL = video.xpath('l:DeliveryUrl', namespaces=CNET_NAMESPACE)[0].text
+      pickedURL = video.xpath('./l:DeliveryUrl', namespaces=CNET_NAMESPACE)[0].text
       pickedBitrate = bitrate
-      
+
   return pickedURL
-  
+
 #######################################
 def pickThumb(images):
   pickedHeight = 0
@@ -91,7 +99,7 @@ def pickThumb(images):
   for image in images:
     height = int(image.get("height"))
     if height > pickedHeight:
-      pickedThumb = image.xpath('l:ImageURL', namespaces=CNET_NAMESPACE)[0].text
+      pickedThumb = image.xpath('./l:ImageURL', namespaces=CNET_NAMESPACE)[0].text
       pickedHeight = height
 
   return pickedThumb
